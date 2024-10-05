081e3b09fd62d61b6d30a4e090210bc9b25df41a
Malek <stonemokkaks@gmail.com>
2024-10-05 19:58:52 +0300

    added backend url

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -34,8 +34,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "http://localhost:5000/api/v1/auth/federated/google/callback",
+      callbackURL: `${process.env.BACKEND_URL}/api/v1/auth/federated/google/callback`,
       scope: ["email", "profile"],
     },
     async (accessToken, refreshToken, profile, done) => {
